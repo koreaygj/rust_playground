7c1261e8e91bb9e881cc9fa5a0af70ab90270ff0
koreaygj <ygj1828@gmail.com>
2024-05-21 10:39:06 +0900

    Study : CH#4 [ownership] String

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
 fn main() {
-    let x = 5;
-    let x = x + 1;
-    {
-        let x = x * 2;
-        println!("The value of x in inner scope is : {x}")
-    }
-    println!("The value of x is : {x}");
-    let guess:u32 = "42".parse().expect("not a number!");
+    let mut s = String::from("hello");
+    s.push_str(", world");
+    println!("{}", s);
 }
