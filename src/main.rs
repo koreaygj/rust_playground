4c0ae8daf22cf1fe8aa7d0744b3475ecf12be911
koreaygj <ygj1828@gmail.com>
2024-07-23 13:13:31 +0900

    Study : CH4 스택 데이터 복사 예제

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,5 @@ mod ownership;
 fn main() {
     ownership::move_function();
     ownership::clone_string();
+    ownership::copy_stack();
 }
--- src/ownership.rs
+++ src/ownership.rs
@@ -12,3 +12,12 @@ pub fn clone_string() {
     let s2 = s1.clone();
     println!("s1 = {}, s2 = {}", s1, s2);
 }
+
+// 스택에 저장되는 데이터는 복사됨
+// 모든 정수형 타입, bool, 모든 부동소수점 타입, 문자 타입
+// copy 가능한 타입만으로 이루어진 튜플 ex) (i32, i32)
+pub fn copy_stack() {
+    let x = 5;
+    let y = x;
+    println!("x = {}, y = {}", x, y);
+}
