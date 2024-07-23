eb27178933688b85a533f5d11e335cd2780ed6e3
koreaygj <ygj1828@gmail.com>
2024-07-23 13:20:48 +0900

    Study : CH4 Ownership function 예제

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,17 @@ fn main() {
     ownership::move_function();
     ownership::clone_string();
     ownership::copy_stack();
+
+    let s = String::from("hello"); // s가 스코프 안으로 들어옵니다
+
+    ownership::takes_ownership(s); // s의 값이 함수로 이동됩니다...
+
+    // ... 따라서 여기서는 더 이상 유효하지 않습니다
+    // println!("{}", s);
+
+    let x = 5; // x가 스코프 안으로 들어옵니다
+    ownership::makes_copy(x); // x가 함수로 이동될 것입니다만,
+
+    // i32는 Copy이므로 앞으로 계속 x를
+    println!("{}", x);
 }
--- src/ownership.rs
+++ src/ownership.rs
@@ -21,3 +21,14 @@ pub fn copy_stack() {
     let y = x;
     println!("x = {}, y = {}", x, y);
 }
+
+pub fn takes_ownership(some_string: String) {
+    // some_string이 스코프 안으로 들어옵니다
+    println!("{}", some_string);
+} // 여기서 some_string이 스코프 밖으로 벗어나고 `drop`이 호출됩니다.
+  // 메모리가 해제됩니다.
+
+pub fn makes_copy(some_integer: i32) {
+    // some_integer가 스코프 안으로 들어옵니다
+    println!("{}", some_integer);
+} // 여기서 some_integer가 스코프 밖으로 벗어납니다. 별다른 일이 발생하지 않습니다.
