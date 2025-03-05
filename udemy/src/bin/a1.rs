8f64729d7d3ee75120671accb26f90b0edf2be51
koreaygj <ygj1828@gmail.com>
2025-03-05 16:19:56 +0900

    add: udemy 예제

---
+++ udemy/src/bin/a1.rs
@@ -0,0 +1,21 @@
+// Topic: Functions
+//
+// Program requirements:
+// * Displays your first and last name
+//
+// Notes:
+// * Use the println macro to display messages to the terminal
+
+// * Use a function to display your first name
+fn display_first_name() {
+    println!("GyeongJin");
+}
+// * Use a function to display your last name
+fn display_last_name() {
+    println!("Yang");
+}
+
+fn main() {
+    display_first_name();
+    display_last_name();
+}
