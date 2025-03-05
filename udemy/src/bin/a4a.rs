9d62598f493a5396b343341361552715aa77eed7
koreaygj <ygj1828@gmail.com>
2025-03-05 21:45:08 +0900

    Sol: match (a4a, a4b)

--- udemy/src/bin/a4a.rs
+++ udemy/src/bin/a4a.rs
@@ -7,4 +7,13 @@
 // * Use a variable set to either true or false
 // * Use a match expression to determine which message to display
 
-fn main() {}
+// * Display "it's true" or "it's false" based on the value of a variable
+fn main() {
+    // * Use a variable set to either true or false
+    let check = true;
+    // * Use a match expression to determine which message to display
+    match check {
+        true => println!("it's true"),
+        false => println!("it's false"),
+    }
+}
--- udemy/src/bin/a4b.rs
+++ udemy/src/bin/a4b.rs
@@ -10,4 +10,18 @@
 // * Use a match expression to determine which message to display
 // * Use an underscore (_) to match on any value
 
-fn main() {}
+// * Display "one", "two", "three", or "other" based on whether
+//   the value of a variable is 1, 2, 3, or some other number,
+//   respectively
+fn main() {
+    // * Use a variable set to any integer
+    let my_int = 2;
+    // * Use a match expression to determine which message to display
+    match my_int {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("tree"),
+        // * Use an underscore (_) to match on any value
+        _ => println!("other"),
+    }
+}
