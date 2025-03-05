fd1bd0e531f22eb3cb3fd4197e5fc05c7f9a69ab
koreaygj <ygj1828@gmail.com>
2025-03-05 21:05:37 +0900

    Sol: basic arithmetic (a2)

--- udemy/src/bin/a2.rs
+++ udemy/src/bin/a2.rs
@@ -8,4 +8,15 @@
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+// * Displays the result of the sum of two numbers
+fn sum(a: i32, b: i32) -> i32 {
+    a + b
+}
+fn display_result(result: i32) {
+    println!("{:?}", result);
+}
+
+fn main() {
+    let result = sum(5, 2);
+    display_result(result);
+}
