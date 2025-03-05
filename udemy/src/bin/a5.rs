487191bc11cf638ced0cc51b8443a1ee302f9e64
koreaygj <ygj1828@gmail.com>
2025-03-05 22:43:54 +0900

    Sol: loop(a5)

--- udemy/src/bin/a5.rs
+++ udemy/src/bin/a5.rs
@@ -5,8 +5,19 @@
 //
 // Notes:
 // * Use a mutable integer variable
-// * Use a loop statement
 // * Print the variable within the loop statement
 // * Use break to exit the loop
 
-fn main() {}
+fn main() {
+    // * Use a mutable integer variable
+    let mut i = 1;
+    // * Use a loop statement
+    loop {
+        println!("{:?}", i);
+        i += 1;
+        // * Use break to exit the loop
+        if i == 5 {
+            break;
+        }
+    }
+}
