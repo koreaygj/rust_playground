d351ba3917d9be6aa2075352897beb3dc1dd6538
koreaygj <ygj1828@gmail.com>
2025-03-05 21:19:30 +0900

    Sol: if, else(a3a)

--- udemy/src/bin/a3a.rs
+++ udemy/src/bin/a3a.rs
@@ -10,4 +10,17 @@
 // * Use an if..else block to determine which message to display
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+// * Displays a message based on the value of a boolean variable
+fn display_message(value: bool) {
+    if value {
+        // * When the variable is set to true, display "hello"
+        println!("hello");
+    } else {
+        // * When the variable is set to false, display "goodbye"
+        println!("goodbye");
+    }
+}
+fn main() {
+    let my_bool = false;
+    display_message(my_bool);
+}
