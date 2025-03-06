6ba8539015355907b3e05598da3269ee1f9176c1
koreaygj <ygj1828@gmail.com>
2025-03-06 20:06:01 +0900

    Sol: expression(a10)

--- udemy/src/bin/a10.rs
+++ udemy/src/bin/a10.rs
@@ -11,5 +11,20 @@
 // * Use a function to print the messages
 // * Use a match expression to determine which message
 //   to print
-
-fn main() {}
+// * Use a function to print the messages
+fn print_message(value: i32) {
+    // * Use a boolean variable set to the result of
+    //   an if..else expression to store whether the value
+    //   is > 100 or <= 100
+    let is_bigger = value > 100;
+    // * Use a match expression to determine which message
+    //   to print
+    match is_bigger {
+        true => println!("its big"),
+        false => println!("its small"),
+    }
+}
+fn main() {
+    let value = 105;
+    print_message(value);
+}
