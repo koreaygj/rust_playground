206a73f4b7a788aea03139b11305a32fa2a67fbe
koreaygj <ygj1828@gmail.com>
2025-03-06 19:44:11 +0900

    Sol: tuple(a10)

--- udemy/src/bin/a9.rs
+++ udemy/src/bin/a9.rs
@@ -7,6 +7,21 @@
 // Notes:
 // * Use a function that returns a tuple
 // * Destructure the return value into two variables
+
+// * Use a function that returns a tuple
+// * Destructure the return value into two variables
+fn coordinate() -> (i32, i32) {
+    (1, 7)
+}
 // * Use an if..else if..else block to determine what to print
 
-fn main() {}
+fn main() {
+    let (x, y) = coordinate();
+    if y > 5 {
+        println!("{:?}, {:?} y value is greater than 5", x, y);
+    } else if y == 5 {
+        println!("{:?}, {:?} y value is equal 5", x, y);
+    } else {
+        println!("{:?}, {:?} y value is less 5", x, y);
+    }
+}
