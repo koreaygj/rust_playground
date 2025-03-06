5879f92894d96c7b5071a83c39e31c052c7d16ec
koreaygj <ygj1828@gmail.com>
2025-03-06 19:15:49 +0900

    Sol: enum(a7)

--- udemy/src/bin/a7.rs
+++ udemy/src/bin/a7.rs
@@ -10,4 +10,26 @@
 // * Use a match expression to determine which color
 //   name to print
 
-fn main() {}
+// * Use an enum with color names as variants
+enum Color {
+    Red,
+    Blue,
+    Green,
+    Yellow,
+}
+// * Use a function to print the color name
+// * The function must use the enum as a parameter
+fn print_color(name: Color) {
+    match name {
+        Color::Red => println!("RED"),
+        Color::Blue => println!("Blue"),
+        Color::Green => println!("Green"),
+        Color::Yellow => println!("Yellow"),
+    }
+}
+fn main() {
+    print_color(Color::Red);
+    print_color(Color::Blue);
+    print_color(Color::Green);
+    print_color(Color::Yellow);
+}
