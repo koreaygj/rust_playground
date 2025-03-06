49d2cecaa236a581460d377fe3d23719bacdfadd
koreaygj <ygj1828@gmail.com>
2025-03-06 19:31:46 +0900

    Sol: Struct & enum(a8)

--- udemy/src/bin/a8.rs
+++ udemy/src/bin/a8.rs
@@ -9,4 +9,32 @@
 // * Use a function to print out the drink flavor and ounces
 // * Use a match expression to print the drink flavor
 
-fn main() {}
+// * Use an enum to create different flavors of drinks
+enum Flavor {
+    Cherry,
+    Watermelon,
+    Apple,
+    Orange,
+}
+// * Use a struct to store drink flavor and fluid ounce information
+struct Drink {
+    flavor: Flavor,
+    fluid_oz: f64,
+}
+// * Use a function to print out the drink flavor and ounces
+fn print_drink(drink: Drink) {
+    // * Use a match expression to print the drink flavor
+    match drink.flavor {
+        Flavor::Apple => println!("Apple , {:?}", drink.fluid_oz),
+        Flavor::Watermelon => println!("Watermelon , {:?}", drink.fluid_oz),
+        Flavor::Orange => println!("Orange , {:?}", drink.fluid_oz),
+        Flavor::Cherry => println!("Cherry , {:?}", drink.fluid_oz),
+    }
+}
+fn main() {
+    let drink = Drink {
+        flavor: Flavor::Apple,
+        fluid_oz: 8.0,
+    };
+    print_drink(drink);
+}
