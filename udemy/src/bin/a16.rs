2d5bf5547f21ff05a1b663e54f997906c13c593b
koreaygj <ygj1828@gmail.com>
2025-03-14 15:56:30 +0900

    Sol: Option a16(25.03.14)

--- udemy/src/bin/a16.rs
+++ udemy/src/bin/a16.rs
@@ -11,4 +11,23 @@
 // * Use a struct containing the student's name and locker assignment
 // * The locker assignment should use an Option<i32>
 
-fn main() {}
+// * Lockers use numbers and are optional for students
+// * Use a struct containing the student's name and locker assignment
+// * The locker assignment should use an Option<i32>
+struct Locker {
+    name: String,
+    locker_num: Option<i32>,
+}
+
+fn main() {
+    let locker = Locker {
+        name: "yang".to_owned(),
+        locker_num: None,
+    };
+    // * Print out the details of a student's locker assignment
+    println!("student name {:?}", locker.name);
+    match locker.locker_num {
+        Some(number) => println!("locker number: {:?}", number),
+        None => println!("no locker assigned"),
+    }
+}
