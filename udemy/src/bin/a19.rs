c59e6664a54c5ad9e83f42e22912c9a355902113
koreaygj <ygj1828@gmail.com>
2025-03-14 18:18:23 +0900

    Sol: Hashmap a19 (25.03.14)

--- udemy/src/bin/a19.rs
+++ udemy/src/bin/a19.rs
@@ -13,4 +13,24 @@
 // Notes:
 // * Use a HashMap for the furniture store stock
 
-fn main() {}
+use std::collections::HashMap;
+
+// * Use a HashMap for the furniture store stock
+fn main() {
+    let mut stock = HashMap::new();
+    stock.insert("Chair", 5);
+    stock.insert("Bed", 3);
+    stock.insert("Table", 2);
+    stock.insert("Couch", 0);
+    let mut total_stock = 0;
+    for (name, value) in stock.iter() {
+        total_stock += value;
+        let stock_count = if *value == 0 {
+            "out of stock".to_owned()
+        } else {
+            format!("{:?}", value)
+        };
+        println!("item = {:?}, stock = {:?} ", name, stock_count);
+    }
+    println!("total stock = {:?}", total_stock);
+}
