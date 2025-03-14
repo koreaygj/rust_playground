c1614a75689c511e0a103a6333e6c58e13d6dbfb
koreaygj <ygj1828@gmail.com>
2025-03-14 18:18:00 +0900

    Sol: Result & ? operator a18b(25.03.14)

--- udemy/src/bin/a18b.rs
+++ udemy/src/bin/a18b.rs
@@ -22,4 +22,54 @@
 // * Print whether the employee may access the building
 //   * Must use a function that utilizes the question mark operator to do this
 
-fn main() {}
+// * Use an enum to represent all types of employees
+enum Position {
+    Maintenance_crew,
+    Marketing_department,
+    Manager,
+    Line_supervisor,
+    Kitchen_staff,
+    Assembly_technician,
+}
+enum Status {
+    Activated,
+    Terminated,
+}
+// * Use a struct to store the employee type and whether they are
+//   still employed
+struct Employee {
+    position: Position,
+    status: Status,
+}
+// * Use a function that returns a Result to determine if the employee
+//   may enter the building
+fn try_access(man: &Employee) -> Result<(), String> {
+    if let Status::Terminated = man.status {
+        return Err("Can't access building(terminated)".to_owned());
+    }
+    match man.position {
+        Position::Maintenance_crew => Ok(()),
+        Position::Marketing_department => Ok(()),
+        Position::Manager => Ok(()),
+        _ => Err("Can't access building(invalid position)".to_owned()),
+    }
+}
+// * Print whether the employee may access the building
+//   * Must use a function that utilizes the question mark operator to do this
+fn print_access(employee: &Employee) -> Result<(), String> {
+    let attempt_access = try_access(employee)?;
+    println!("access ok");
+    Ok(())
+}
+
+fn main() {
+    let man = &Employee {
+        position: Position::Manager,
+        status: Status::Activated,
+    };
+    let access = print_access(&man);
+    match access {
+        Err(e) => println!("{:?}", e),
+        _ => (),
+    }
+}
