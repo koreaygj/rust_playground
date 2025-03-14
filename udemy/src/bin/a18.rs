5df86fb049941b5a58368c05ff16f9fa6d1c51c3
koreaygj <ygj1828@gmail.com>
2025-03-14 16:40:18 +0900

    Sol: Result a18(25.03.14)

--- udemy/src/bin/a18.rs
+++ udemy/src/bin/a18.rs
@@ -11,4 +11,28 @@
 // * Return a result from the function
 // * The Err variant should detail the reason why they cannot make a purchase
 
-fn main() {}
+// * Use a struct to store at least the age of a customer
+// * Determine if a customer is able to make a restricted purchase
+#[derive(Debug)]
+struct Customer {
+    name: String,
+    age: i32,
+}
+// * Use a function to determine if a customer can make a restricted purchase
+// * Return a result from the function
+// * The Err variant should detail the reason why they cannot make a purchase
+fn check_age(customer: &Customer) -> Result<(), String> {
+    if customer.age >= 21 {
+        Ok(())
+    } else {
+        Err("customer must be at least 21 years old...".to_owned())
+    }
+}
+fn main() {
+    let customer = Customer {
+        name: "yang".to_owned(),
+        age: 20,
+    };
+    let result = check_age(&customer);
+    print!("{:?}", result);
+}
